ecd4996d0093740dfaf35b6506f4634238143e31
Jamie Reynolds <32643211+JamieRPR@users.noreply.github.com>
2021-03-17 19:26:24 +0000

    Add files via upload

---
+++ linked-list/LinkListGen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_LinkedListGen
+{
+    class LinkListGen<T> where T : IComparable
+    {
+        private LinkGen<T> list;
+
+        public LinkListGen()
+        {
+        }
+        public void AddItem(T item)
+        {
+        }
+        public string DisplayList()
+        {
+        }
+        public int NumberOfItems()
+        {
+        }
+        public bool IsPresentItem(T item)
+        {
+        }
+        public void RemoveItem(T item)
+        {
+        }
+    }
+
